b04b95806cfc756d0b46a795d86628f2ef3e2856
SalahAltamimi <salahabufarha1996@outlook.com>
2024-07-30 08:08:07 +0300

    final

--- src/services/apiMovies.js
+++ src/services/apiMovies.js
@@ -2,14 +2,14 @@ import axios from "axios";
 export async function apiMovies(query) {
   const data = await axios({
     method: "GET",
-    url: `http://www.omdbapi.com/?apikey=53f9eba3&s=${query}`,
+    url: `https://www.omdbapi.com/?apikey=53f9eba3&s=${query}`,
   });
   return data;
 }
 export async function apiMovie(select) {
   const data = await axios({
     method: "GET",
-    url: `http://www.omdbapi.com/?apikey=53f9eba3&i=${select}`,
+    url: `https://www.omdbapi.com/?apikey=53f9eba3&i=${select}`,
   });
   return data;
 }
